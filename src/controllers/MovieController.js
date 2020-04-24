a16e24fe7aa1af16fbcd5f0d7cfbe1d5c7624fd2
madekot <mgolubcov@gmail.com>
2020-04-24 16:00:05 +0300

    Создаст контроллер задачи

---
+++ src/controllers/MovieController.js
@@ -0,0 +1,11 @@
+import FilmCardComponent from "../components/film-card";
+import FilmDetails from "../components/film-details";
+import {render, remove} from "../utils/render";
+
+export default class TaskController {
+  constructor(container) {
+  }
+
+  render(card) {
+  }
+}
